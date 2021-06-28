6f7d63ce248b2eb70498a5a0383bee90984ddfb5
coffeeandpouch <sameinsidler@me.com>
2021-06-28 10:36:16 -0400

    commits modified SearchForm

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -1,2 +1,15 @@
-import React from "react";
 import "./SearchForm.css";
+
+function SearchForm() {
+  return (
+    <form>
+      <label>
+        City:
+        <input type="text" name="city" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default SearchForm;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import SearchForm from "./SearchForm";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <SearchForm />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
